39f7d3b7183273c1def0ca097fb1d67ed8712bf0
dhkaneda <dennishuynh@protonmail.com>
2021-10-07 14:38:18 -0700

    Generates possible daemon sequences, externalize game config

---
+++ config.js
@@ -0,0 +1,66 @@
+export const config = {
+  byteRange: ['BD', 'E9', '55', '1C', '7A'],
+  maxBufferSize: 8,
+  matrixSize: 5,
+  possibleDaemons: [
+    "Mass Vulnerability",
+    "Camera Shutdown",
+    "ICEPICK",
+  ],
+  bufferKey: {
+    2: [
+      {
+        bufferSize: 4,
+        split: [[0, 2], [2, 2]]
+      },
+      {
+        bufferSize: 4,
+        split: [[0, 3], [2, 2]]
+      },
+      {
+        bufferSize: 4,
+        split: [[0, 2], [1, 3]]
+      },
+      {
+        bufferSize: 5,
+        split: [[0, 2], [1, 4]]
+      },
+      {
+        bufferSize: 5,
+        split: [[0, 3], [3, 2]]
+      },
+      {
+        bufferSize: 5,
+        split: [[0, 4], [3, 2]]
+      },
+      {
+        bufferSize: 6,
+        split: [[0, 3], [3, 3]]
+      },
+    ],
+    3: [
+        {
+          bufferSize: 6, 
+          split: [[0, 3], [2, 2], [3, 3]],
+        }, 
+        {
+          bufferSize: 7, 
+          split: [[0, 3], [2, 3], [4, 3]],
+        },
+        {
+          bufferSize: 7, 
+          split: [[0, 3], [2, 3], [3, 4]],
+        }, 
+        {
+          bufferSize: 8, 
+          split: [[0, 3], [2, 3], [4, 4]],
+        },
+        {
+          bufferSize: 8, 
+          split: [[0, 3], [2, 3], [5, 3]],
+        },
+      ],
+    // 4: [],
+    // 5: [],
+  }
+};
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import { BreachProtocol } from './lib/game/BreachProtocol.js';
+import { config } from './config.js';
 
-const matrix = new BreachProtocol();
+const { maxBufferSize, matrixSize, byteRange } = config;
+
+const matrix = new BreachProtocol(maxBufferSize, matrixSize, byteRange);
 
 matrix.init();
 
