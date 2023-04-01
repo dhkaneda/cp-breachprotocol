a5f345fa388a5022760a3378d5ec9210d12de976
Dennis Huynh <dennis.huynh@galvanize.com>
2023-03-31 20:44:10 -0700

    daemonSequences generated properly with random order

--- config.js
+++ config.js
@@ -4,7 +4,29 @@ export const config = {
   possibleDaemons: [
     "Mass Vulnerability",
     "Camera Shutdown",
-    "ICEPICK",
+    "Contagion",
+    "Short Circuit",
+    "Ping",
+    "Overheat",
+    "Reboot Optics",
+    "Sonic Shock",
+    "Whistle",
+    "Weapon Glitch",
+    "Data Mine",
+    "Memory Wipe",
+    "Remote Deactivation",
+    "Netdriver",
+    "Distract Enemies",
+    "Armor Hack",
+    "Firewall Breach",
+    "Quickhack Charge",
+    "System Reset",
+    "Subdermal Sensor",
+    "Datamine",
+    "Firewall Penetration",
+    "Data Corruption",
+    "Data Theft",
+    "Backdoor",
   ],
   bufferKey: {
     2: [
@@ -59,7 +81,71 @@ export const config = {
           subSequence: [[0, 3], [2, 3], [5, 3]],
         },
       ],
-    // 4: [],
-    // 5: [],
+    4:  [
+        {
+          size: 8,
+          subSequence: [[0, 3], [2, 3], [4, 3], [5, 3]],
+        },
+        {
+          size: 9,
+          subSequence: [[0, 3], [2, 3], [4, 3], [6, 3]],
+        },
+        {
+          size: 9,
+          subSequence: [[0, 3], [2, 3], [4, 3], [5, 4]],
+        },
+        {
+          size: 10,
+          subSequence: [[0, 3], [2, 3], [4, 3], [6, 4]],
+        },
+        {
+          size: 10,
+          subSequence: [[0, 3], [2, 3], [4, 3], [6, 3], [7, 3]],
+        },
+      ],
+    5: [
+        {
+          size: 10,
+          subSequence: [[0, 3], [2, 3], [4, 3], [6, 3], [8, 2]],
+        },
+        {
+          size: 11,
+          subSequence: [[0, 3], [2, 3], [4, 3], [6, 3], [8, 3]],
+        },
+        {
+          size: 11,
+          subSequence: [[0, 3], [2, 3], [4, 3], [6, 3], [8, 3], [9, 2]],
+        },
+        {
+          size: 12,
+          subSequence: [[0, 3], [2, 3], [4, 3], [6, 3], [8, 3], [10, 2]],
+        },
+        {
+          size: 12,
+          subSequence: [[0, 3], [2, 3], [4, 3], [6, 3], [8, 4]],
+        },
+      ],
+    6: [
+      {
+        size: 12,
+        subSequence: [[0, 3], [2, 3], [4, 3], [6, 3], [8, 5]]
+      },
+      {
+        size: 13,
+        subSequence: [[0, 3], [2, 3], [4, 3], [6, 3], [8, 4], [10, 3]]
+      },
+      {
+        size: 13,
+        subSequence: [[0, 3], [2, 3], [4, 3], [6, 3], [8, 4], [11, 2]]
+      },
+      {
+        size: 14,
+        subSequence: [[0, 3], [2, 3], [4, 3], [6, 3], [8, 4], [11, 3]]
+      },
+      {
+        size: 14,
+        subSequence: [[0, 3], [2, 3], [4, 3], [6, 3], [8, 4], [11, 3], [12, 2]]
+      }
+    ]
   }
 };
--- index.js
+++ index.js
@@ -3,7 +3,11 @@ import { selectRandomFrom } from './lib/utils.js';
 import { config } from './config.js';
 
 const { matrixSize, byteRange } = config;
-const bufferParams = selectRandomFrom(config.bufferKey[selectRandomFrom(Object.keys(config.bufferKey))]);
+
+const bufferKeys = Object.keys(config.bufferKey);
+const randomBufferKeyIndex = selectRandomFrom(bufferKeys);
+const bufferParams = selectRandomFrom(config.bufferKey[randomBufferKeyIndex]);
+
 
 const matrix = new BreachProtocol(matrixSize, byteRange, bufferParams);
 
